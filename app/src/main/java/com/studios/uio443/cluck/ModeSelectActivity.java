1a083df2f6ae6f5b1484dbb8eac78719ed4f3417
Uio443 <ronaldkirk101pc@gmail.com>
2018-06-09 14:23:10 -0400

    Добавлен экран выбора вопроса или ответа.

--- app/src/main/java/com/studios/uio443/cluck/ModeSelectActivity.java
+++ app/src/main/java/com/studios/uio443/cluck/ModeSelectActivity.java
@@ -2,6 +2,8 @@ package com.studios.uio443.cluck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ModeSelectActivity extends AppCompatActivity {
 
@@ -10,4 +12,7 @@ public class ModeSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_select);
     }
+
+
+
 }
