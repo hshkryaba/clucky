36414fb4d8aa70f374786182e204301acdf3bdd5
Uio443 <ronaldkirk101pc@gmail.com>
2018-06-18 18:52:20 -0400

    About Page Added

---
+++ app/src/main/java/com/studios/uio443/cluck/AboutPage.java
@@ -0,0 +1,13 @@
+package com.studios.uio443.cluck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+}
