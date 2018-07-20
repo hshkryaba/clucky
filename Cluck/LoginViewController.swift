4a246c32ef8b74d157336d89c84d10edbefecc9f
Riley Usagi <riley.usagi@gmail.com>
2018-07-20 12:19:19 +0300

    Fix url

--- Cluck/LoginViewController.swift
+++ Cluck/LoginViewController.swift
@@ -13,9 +13,10 @@ class LoginViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Temp
-    emailTextField.text     = "info@info.com"
-    passwordTextField.text  = "123456"
+    // Temporary for testing registeration page
+    // emailTextField.text     = "info@info.com"
+    // passwordTextField.text  = "123456"
+    // nameTextField.text      = "someExample"  
     
     nameTextField.isHidden = true
     loginButton.layer.cornerRadius = 5
@@ -59,10 +60,10 @@ class LoginViewController: UIViewController {
   
   // Процесс Авторизации/Регистрации при нажатии на кнопку Войти/Зарегистрироваться
   @IBAction func tapEnterButton(_ sender: Any) {
-
+    
     // Обозначение контролёра, к которому будет совершён переход по окончании автоизационного замыкания
     let controller = self.storyboard?.instantiateViewController(withIdentifier: "QuestionListTableViewController") as! QuestionListTableViewController
-
+    
     if nameTextField.isHidden {
       // Авторизация
       app.api.login(username: emailTextField.text!, password: passwordTextField.text!, completion: {
@@ -71,7 +72,7 @@ class LoginViewController: UIViewController {
         // Процесс перехода на указанный выше контролёр
         self.present(controller, animated:true, completion:nil)
       })
-
+      
     } else {
       // Регистрация
       app.api.signup(email: emailTextField.text!, login: nameTextField.text!, password: passwordTextField.text!, completion: {
@@ -83,37 +84,37 @@ class LoginViewController: UIViewController {
     }
     
     // TODO: credentials encoded in base64
-//    let username = emailTextField.text!
-//    let password = passwordTextField.text!
-//    
-//    let Url = String(format: "http://cluck-app.org/api/auth/login")
-//    guard let serviceUrl = URL(string: Url) else { return }
-//    
-//    // let loginParams = String(format: LOGIN_PARAMETERS1, "test", "Hi World")
-//    
-//    let parameterDictionary = ["login" : username, "password" : password]
-//    var request = URLRequest(url: serviceUrl)
-//    request.httpMethod = "POST"
-//    request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-//    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameterDictionary, options: []) else {
-//      return
-//    }
-//    request.httpBody = httpBody
-//    
-//    let session = URLSession.shared
-//    session.dataTask(with: request) { (data, response, error) in
-//      if let response = response {
-//        magic(response)
-//      }
-//      if let data = data {
-//        do {
-//          let json = try JSONSerialization.jsonObject(with: data, options: [])
-//          magic(json)
-//        }catch {
-//          magic(error)
-//        }
-//      }
-//      }.resume()
+    //    let username = emailTextField.text!
+    //    let password = passwordTextField.text!
+    //    
+    //    let Url = String(format: "http://cluck-app.org/api/auth/login")
+    //    guard let serviceUrl = URL(string: Url) else { return }
+    //    
+    //    // let loginParams = String(format: LOGIN_PARAMETERS1, "test", "Hi World")
+    //    
+    //    let parameterDictionary = ["login" : username, "password" : password]
+    //    var request = URLRequest(url: serviceUrl)
+    //    request.httpMethod = "POST"
+    //    request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+    //    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameterDictionary, options: []) else {
+    //      return
+    //    }
+    //    request.httpBody = httpBody
+    //    
+    //    let session = URLSession.shared
+    //    session.dataTask(with: request) { (data, response, error) in
+    //      if let response = response {
+    //        magic(response)
+    //      }
+    //      if let data = data {
+    //        do {
+    //          let json = try JSONSerialization.jsonObject(with: data, options: [])
+    //          magic(json)
+    //        }catch {
+    //          magic(error)
+    //        }
+    //      }
+    //      }.resume()
   }
   
   /// Забыл пароль, друг?
